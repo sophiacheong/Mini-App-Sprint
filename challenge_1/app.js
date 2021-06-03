fc96d09c4c510d423dcb6b20e8647df8b5f16e93
sophiacheong <sophiaacheong@gmail.com>
2021-06-02 22:15:03 -0700

    finished basic min. req

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,16 @@
+function startGame() {
+  document.turn = 'X';
+}
 
+function nextMove(square) {
+  square.innerText = document.turn;
+  switchTurn();
+}
 
+function switchTurn() {
+  if (document.turn == 'X') {
+    document.turn = 'O';
+  } else {
+    document.turn = 'X';
+  }
+}
