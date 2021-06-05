ffab1e7510908bb24b07d5fba3f912851021013e
sophiacheong <sophiaacheong@gmail.com>
2021-06-04 22:58:56 -0700

    adjusted server.js file with app.post

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -14,30 +14,40 @@ app.use(cors());
 app.use(bodyparser.json());
 
 app.use(express.static(PATH.join(__dirname, './client')))
-app.use('/api', router);
+// app.use('/', router);
+
+app.post('/upload_json', (req, res) => {
+  (err, results) => {
+    if (err) {
+      res.sendStatus(404).send(err);
+    } else {
+      res.sendStatus(200).send('Success!');
+    }
+  }
+})
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
 
-router
-  .route('/upload_json')
-  .get((req, res) => {
-    (err, results) => {
-      if (err) {
-        res.sendStatus(404).send(err);
-      } else {
-        res.sendStatus(200).send(results);
-      }
-    }
-  })
-  .post((req, res) => {
-    (err, results) => {
-      if (err) {
-        res.sendStatus(404).send(err);
-      } else {
-        res.sendStatus(200).send('successful!');
-      }
-    }
-  })
+// router
+//   .route('/upload_json')
+//   .get((req, res) => {
+//     (err, results) => {
+//       if (err) {
+//         res.sendStatus(404).send(err);
+//       } else {
+//         res.sendStatus(200).send(results);
+//       }
+//     }
+//   })
+//   .post((req, res) => {
+//     (err, results) => {
+//       if (err) {
+//         res.sendStatus(404).send(err);
+//       } else {
+//         res.sendStatus(200).send('successful!');
+//       }
+//     }
+//   })
 
 // The server must flatten the JSON hierarchy, mapping each item/object in the JSON to a single line of CSV report (see included sample output), where the keys of the JSON objects will be the columns of the CSV report.
 // You may assume the JSON data has a regular structure and hierarchy (see included sample file). In other words, all sibling records at a particular level of the hierarchy will have the same set of properties, but child objects might not contain the same properties. In all cases, every property you encounter must be present in the final CSV output.
