d44f2b484c99a54c4177600029a5c7c583b0bed2
sophiacheong <sophiaacheong@gmail.com>
2021-06-23 16:42:58 -0700

    added file data client side

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,14 @@
+var fileData = function(e) {
+  e.preventDefault();
+  var textArea = document.getElementById('text').value;
+  var fileData = document.getElementById('fileData').files[0];
+  var keyword = document.getElementById('keyword').value;
 
+  var formData = new FormData();
+  formData.append('textArea', textArea);
+  formData.append('fileData', fileData);
+  formData.append('keyword', keyword);
+
+  sendFile(formData);
+}
 
