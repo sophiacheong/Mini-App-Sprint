6f0478ef2e204665e36be3aa3cf4bb8314c4192d
sophiacheong <sophiaacheong@gmail.com>
2021-06-23 16:58:41 -0700

    changed html format

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -2,12 +2,12 @@ var fileData = function(e) {
   e.preventDefault();
   var textArea = document.getElementById('text').value;
   var fileData = document.getElementById('fileData').files[0];
-  var keyword = document.getElementById('keyword').value;
+  // var keyword = document.getElementById('keyword').value;
 
   var formData = new FormData();
   formData.append('textArea', textArea);
   formData.append('fileData', fileData);
-  formData.append('keyword', keyword);
+  // formData.append('keyword', keyword);
 
   sendFile(formData);
 }
