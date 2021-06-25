2602404d0a21ffbbb9759aa65558ba8ba4451351
sophiacheong <sophiaacheong@gmail.com>
2021-06-24 20:53:41 -0700

    got rid of typo

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -25,48 +25,10 @@ const uid = 0;
 const fileHeader = [];
 
 app.post('/convert', upload.single('fileData'), (req, res) => {
-  fs.open(PATH.join(__dirname, 'data.csv'), 'w', () => {
-    let body = req.body;
-    console.log(body)
-    if (body[body.length - 1] === ';') {
-      body = body.slice(0, -1)
-    };
-    let data = JSON.parse(body);
-    let sep = ',';
-    let header = '';
-    for (key in data) {
-      if (key !== 'children') {
-        header = header + key + sep;
-      };
-    };
-    header = header.slice(0, -1) + '\n';
 
-    fs.appendFile('data.csv', header, () => {
-      let rows = '';
-      let recurse = function(data) {
-        if (!data.children) {
-          return;
-        }
 
-        let row = '';
-        for (key in data) {
-          if (key !== 'children') {
-            row = row + data[key] + sep;
-          }
-        }
-        row = row.slice(0, -1) + '\n';
-        rows = row + row;
-        for (let i = 0; i < data.children.length; i++) {
-          recurse(data.children[i]);
-        }
-      }
-      recurse(data);
-      fs.appendFile('data.csv', rows, () => {
-        fs.readFile(PATH.join(__dirname, 'data.csv'), (err, data) => res.send(data));
-      });
-    });
-  });
-});
+  };
+);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
 
