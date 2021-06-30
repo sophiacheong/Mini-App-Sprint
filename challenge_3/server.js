e9cb4d6e60037f10c5a821521b2ed3f75edc4a56
sophiacheong <sophiaacheong@gmail.com>
2021-06-29 22:34:35 -0700

    fixed compile to watch all files under client

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const path = require('path');
 const PORT = 3000;
 
 const app = express();
@@ -9,6 +10,6 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.use(express.static('/public'))
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
