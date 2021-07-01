41fcfd834ea140e4650a0da25fb95823622bfbae
sophiacheong <sophiaacheong@gmail.com>
2021-06-30 17:25:42 -0700

    created schema

--- challenge_3/db/index.js
+++ challenge_3/db/index.js
@@ -2,3 +2,16 @@ const mongoose = require('mongoose');
 const db = mongoose.connection;
 
 mongoose.connect('mongodb://localhost/fetcher', { useNewUrlParser: true, useUnifiedTopology: true });
+
+let repo = mongoose.Schema({
+  id: Number,
+  name: String,
+  email: String,
+  password: String,
+  address: String,
+  phone_number: Number,
+  card_number: Number,
+  expiration_date: Number,
+  CVV: Number,
+  zip_code: Number
+})
